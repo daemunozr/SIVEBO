5745e036d2807a378dd4f1cf8073a59566e9cfc0
daemunozr <dae.munozr@duocuc.cl>
2026-04-22 20:58:46 -0400

    sivevbo app creado

---
+++ src/main/java/com/example/sivebo/SiveboApplication.java
@@ -0,0 +1,13 @@
+package com.example.sivebo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiveboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiveboApplication.class, args);
+	}
+
+}
